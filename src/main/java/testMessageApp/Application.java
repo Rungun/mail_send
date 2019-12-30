37f8b8ecb01ebf8946f37990dde53766c973c6f1
vmukhametzyanov <Rungun>
2019-12-30 16:44:01 +0300

    test

---
+++ src/main/java/testMessageApp/Application.java
@@ -0,0 +1,13 @@
+package testMessageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//123$5rewq
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+
+    }
+}
